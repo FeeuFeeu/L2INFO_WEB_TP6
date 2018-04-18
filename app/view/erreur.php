15160a6903ee3e05890290a1179ded32fc34b2f5
FeeuFeeu <agneray.alexandre@hotmail.fr>
2018-04-18 11:53:57 +0200

    Add files via upload

---
+++ app/view/erreur.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <div class="container">
+		<p class="text-left" style="font-size:6em;">Oups...</p> <br />
+		<p class="text-left" style="font-size:4em;">La page demandée n'existe pas ou a renvoyé une erreur.</p>
+    </div>
+  </body>
+</html>
+
---
+++ app/view/head.php
@@ -0,0 +1,16 @@
+<!-- EN-TETE -->
+<head>
+	<meta charset="utf-8">
+	<title> <?php echo "LEGO Webstore " . $data['name']; ?> </title>
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css"/>
+	<link rel="stylesheet" href="public/css/styles.css"/>
+	<!-- JavaScripts -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	<script src="public/js/produit.js"></script>
+</head>
+
+
