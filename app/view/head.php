725271c20ecb46bb576e696c982ab723c9d27cc3
FeeuFeeu <agneray.alexandre@hotmail.fr>
2018-04-19 08:48:36 +0200

    Add files via upload

--- app/view/head.php
+++ app/view/head.php
@@ -3,13 +3,13 @@
 	<meta charset="utf-8">
 	<title> <?php echo "LEGO Webstore " . $data['name']; ?> </title>
 	<!-- CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css"/>
+	<link rel="stylesheet" href="public/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="public/css/open-iconic-bootstrap.min.css"/>
 	<link rel="stylesheet" href="public/css/styles.css"/>
 	<!-- JavaScripts -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	<script src="public/js/jquery.min.js"></script>
+	<script src="public/js/popper.min.js"></script>
+	<script src="public/js/bootstrap.min.js"></script>
 	<script src="public/js/produit.js"></script>
 </head>
 
